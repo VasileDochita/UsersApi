03fa593d9467bda4b4b1dc554e610df8696b4091
dochitavasile1987 <dochitavasile1987@gmail.com>
2020-02-07 19:25:37 +0200

    1

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using UsersApi.Models;
+
+namespace UsersApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        // GET api/users
+        [Route("")]
+        [HttpGet]
+
+        public IActionResult Get()
+        {
+            var list = new List<User>
+            {
+                new User { Id = 1, City = "Iasi", Email = "vasile@whatever.com", Username = "Vasile" },
+                new User { Id = 2, City = "Bucuresti", Email="alexandra@whatever.com", Username = "alexandra"}
+            };
+            return this.Ok(list) ;
+        }
+     
+               
+    }
+}
---
+++ Models/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UsersApi.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+        public string Username { get; set; }
+
+        public string Email { get; set; }
+
+        public string Description { get; set; }
+
+        public string City { get; set; }
+
+        public string Street { get; set; }
+    }
+}
