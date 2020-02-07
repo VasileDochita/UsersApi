68a3d81cbba945495a7ff1f997e3e89429efbbc0
dochitavasile1987 <dochitavasile1987@gmail.com>
2020-02-07 20:41:46 +0200

    3

--- Models/User.cs
+++ Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,9 +9,12 @@ namespace UsersApi.Models
     public class User
     {
         public int Id { get; set; }
-
+        
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [EmailAddress]
         public string Email { get; set; }
 
         public string Description { get; set; }
